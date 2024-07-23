6cc1062231827a80829b261f88b3b842282df41e
Matheus dos Santos <149119348+MS12112@users.noreply.github.com>
2024-07-23 18:53:02 -0300

    Update ProjetoBackup.py

--- ProjetoBackup.py
+++ ProjetoBackup.py
@@ -1,34 +1,28 @@
-#Bibliotecas
+# janela para selecionar a pasta do nosso computador
 import os
 from tkinter.filedialog import askdirectory
 import shutil
 import datetime
 
-# Solicita ao usuário que selecione a pasta para fazer o backup
 nome_pasta_selecionada = askdirectory()
 
-# Obtém a lista de arquivos e pastas na pasta selecionada
 lista_arquivos = os.listdir(nome_pasta_selecionada)
 
-# Define o nome e o caminho da pasta de backup
+# fazer o backup dos arquivos que estão nessa pasta
 nome_pasta_backup = "backup"
 nome_completo_pasta_backup = f"{nome_pasta_selecionada}/{nome_pasta_backup}"
-
-# Cria a pasta de backup se ela não existir
 if not os.path.exists(nome_completo_pasta_backup):
     os.mkdir(nome_completo_pasta_backup)
 
-# Obtém a data e hora atuais para criar uma subpasta com data e hora
 data_atual = datetime.datetime.today().strftime("%Y-%m-%d %H%M%S")
 
-# Itera sobre cada arquivo e pasta na pasta selecionada
 for arquivo in lista_arquivos:
     nome_completo_arquivo = f"{nome_pasta_selecionada}/{arquivo}"
     nome_final_arquivo = f"{nome_completo_pasta_backup}/{data_atual}/{arquivo}"
     
-    # Verifica se o item é um arquivo ou uma pasta e faz o backup apropriado
     if not os.path.exists(f"{nome_completo_pasta_backup}/{data_atual}"):
         os.mkdir(f"{nome_completo_pasta_backup}/{data_atual}")
+    
     if "." in arquivo:
         shutil.copy2(nome_completo_arquivo, nome_final_arquivo)
     elif "backup" != arquivo:
